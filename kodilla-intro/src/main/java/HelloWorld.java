f0b0da546e9751aa9cfd34eeb12633f58640404c
Piotr Topolski <piter134@gmail.com>
2020-11-15 10:46:14 +0100

    Hello World

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World!"); // [3]
+        System.out.println("Piotr"); //[4]
+        System.out.println("27lat"); //[5]
+        System.out.println("T"); //[6]
+    }
+}
