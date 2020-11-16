59bdec89e473bf78d2ed498d10b3d239983c053c
Piotr Topolski <piter134@gmail.com>
2020-11-16 10:41:22 +0100

    Zadanie nr 2

---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Piotr";
+        int myAge = 27;
+        double height = 1.83;
+        char lastnameLetter = 'T';
+        boolean rankPlatinum = true;
+        boolean rankDiamond = false;
+        String myPassion = "Aquascaping";
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(lastnameLetter);
+        System.out.println(rankPlatinum);
+        System.out.println(rankDiamond);
+        System.out.println(myPassion);
+    }
+}
