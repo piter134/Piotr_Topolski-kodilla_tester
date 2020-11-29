e0df1c431e7df47ac4e2ebbefcb37c7497c34d17
Piotr Topolski <piter134@gmail.com>
2020-11-29 20:46:02 +0100

    LeapYear

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,7 +1,9 @@
 public class LeapYear {
     public static void main(String[] args) {
-
-        isLeapYear(2009);
+        int[] years = new int[] {2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010};
+        for(int i = years.length -1;i >= 0; i--) {
+            isLeapYear(years[i]);
+        }
     }
     private static boolean isLeapYear(int year){
         if (year % 4 == 0) {
