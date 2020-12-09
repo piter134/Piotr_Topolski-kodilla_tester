4391f2a8020777572c21bae5d53a20afecdc9c84
Piotr Topolski <piter134@gmail.com>
2020-12-09 21:04:22 +0100

    Notebook

---
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -0,0 +1,24 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        Notebook notebook = new Notebook(600, 500, 1992);
+        System.out.println(notebook.weight + "g");
+        notebook.checkWeight();
+        System.out.println(notebook.price);
+        notebook.checkPrice();
+        System.out.println(notebook.year + "r");
+
+        Notebook heavyNotebook = new Notebook(2000,900, 2009);
+        System.out.println(heavyNotebook.weight + "g");
+        heavyNotebook.checkPrice();
+        System.out.println(heavyNotebook.price);
+        heavyNotebook.checkWeight();
+        System.out.println(heavyNotebook.year + "r");
+
+        Notebook oldNotebook = new Notebook(1000, 1600, 2019);
+        System.out.println(oldNotebook.price);
+        oldNotebook.checkPrice();
+        System.out.println(oldNotebook.weight + "g");
+        oldNotebook.checkWeight();
+        System.out.println(oldNotebook.year + "r");
+    }
+}
---
+++ kodilla-intro/src/main/java/Notebook.java
@@ -0,0 +1,31 @@
+import java.awt.desktop.SystemSleepEvent;
+
+public class Notebook {
+    int weight;
+    int price;
+    int year;
+    public Notebook(int weight, int price, int year) {
+        this.weight = weight;
+        this.price = price;
+        this.year = year;
+    }
+
+    public void checkPrice() {
+        if (this.price < 600 && this.year < 1993) {
+            System.out.println("This notebook is crap.");
+        } else if (this.price < 1000 && this.year < 2010) {
+            System.out.println("This note is quite ok.");
+        } else {
+            System.out.println("This notebook is damn good.");
+        }
+    }
+    public void checkWeight() {
+        if (this.weight < 600) {
+            System.out.println("This device light");
+        } else if (this.weight <= 1000) {
+            System.out.println("This device is quite heavy");
+        } else {
+            System.out.println("This device is fucking heavy");
+        }
+    }
+}
