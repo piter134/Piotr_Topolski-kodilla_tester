093b3833751e13304ccd6126abae42e00a101982
Piotr Topolski <piter134@gmail.com>
2020-12-15 18:07:22 +0100

    application

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -1,24 +1,9 @@
 public class Application {
-    String name;
-    double age;
-    double height;
-
-    public Application(String name, double age, double height) {
-        this.name = name;
-        this.age = age;
-        this.height = height;
-    }
-
-    public void applicationResoult() {
-        if (name != null) {
-            if (age > 30 && height > 160) {
-                System.out.println("User is older than 30 and higher than 160cm");
-            } else {
-                System.out.println("User is 30 (or younger) or 160cm (or shorter)");
-            }
-
-        }
-    }
+    public static void main(String[] args) {
+    User user = new User("adam", 40.5, 178);
+        System.out.println(user.name);
+        System.out.println(user.age);
+        System.out.println(user.height);
+        user.userInformation();
 }
-
-
+}
--- kodilla-intro/src/main/java/User.java
+++ kodilla-intro/src/main/java/User.java
@@ -1,9 +1,23 @@
 public class User {
-    public String name;
-    public int age;
+    String name;
+    double age;
+    double height;
 
-    public User(String name , int age){
+    public User(String name, double age, double height) {
         this.name = name;
         this.age = age;
+        this.height = height;
+    }
+
+    public void userInformation() {
+        if (name != null) {
+            if (age > 30 && height > 160) {
+                System.out.println("User is older than 30 and higher than 160cm");
+            } else {
+                System.out.println("User is 30 (or younger) or 160cm (or shorter)");
+            }
+
+        }
     }
 }
+
