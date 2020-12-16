b06233ed1f625fccfb4814f903cc7554b7cbfbc3
Piotr Topolski <piter134@gmail.com>
2020-12-16 19:13:05 +0100

    added missing code

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -1,2 +1,10 @@
 public class Book {
+    private String author;
+    private String title;
+    public static Book of(String author, String title){
+        Book book = new Book();
+        book.author = author;
+        book.title = title;
+        return book;
+    }
 }
