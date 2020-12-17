27187f53731b85f07658fb2315501af4a2a929ea
Piotr Topolski <piter134@gmail.com>
2020-12-17 20:48:26 +0100

    zadanie 2.5 - zadanie 2

---
+++ kodilla-intro/src/main/java/RandomNumberInitialization.java
@@ -0,0 +1,6 @@
+public class RandomNumberInitialization {
+    public static void main(String[] args) {
+        RandomNumbers randomNumbers = new RandomNumbers();
+        randomNumbers.startGeneratingNumbers();
+    }
+}
---
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+public class RandomNumbers {
+    Random random = new Random();
+    public void startGeneratingNumbers(){
+        int sum;
+        int lowestRandomNumber = 30;
+        int highestRandomNumber = 0;
+
+        for (sum = 0; sum < 5000;){
+            int randomNumber = getRandomNumber(31);
+            sum = sum + randomNumber;
+            if(randomNumber < lowestRandomNumber){
+                lowestRandomNumber = randomNumber;
+            }
+            else if(randomNumber > highestRandomNumber){
+                highestRandomNumber = randomNumber;
+            }
+
+        }
+        System.out.println(sum);
+        System.out.println("lowest generated number is " + lowestRandomNumber);
+        System.out.println("highest generated number is " + highestRandomNumber);
+
+    }
+    public int getRandomNumber(int upperBound){
+        int randomNumber = random.nextInt(upperBound);
+        return randomNumber;
+    }
+}
