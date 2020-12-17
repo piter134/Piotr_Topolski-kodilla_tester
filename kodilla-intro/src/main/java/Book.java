ad11d63d23bb5f622f6b404adce1b97f3b982a17
Piotr Topolski <piter134@gmail.com>
2020-12-17 19:35:43 +0100

    zadanie 2.7

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -1,10 +1,12 @@
 public class Book {
     private String author;
     private String title;
+    public Book(String author,String title) {
+        this.author = author;
+        this.title = title;
+    }
     public static Book of(String author, String title){
-        Book book = new Book();
-        book.author = author;
-        book.title = title;
+        Book book = new Book(author, title);
         return book;
     }
 }
